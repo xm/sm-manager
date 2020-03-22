b272f95043c237184cfb91260e81b5e7ad8e4025
xm <xianmoniz@gmail.com>
2020-03-22 01:34:26 -0700

    Refactor out parse_songs_file from import_songs

--- lib/importer.py
+++ lib/importer.py
@@ -2,13 +2,31 @@ import csv
 
 
 def import_songs(songs_file):
-    # parse songs_file as tsv
-    parsed = list(csv.DictReader(songs_file, delimiter="\t", quotechar='"'))
+    songs_to_download = parse_songs_file(songs_file)
+    total = len(songs_to_download)
 
-    if len(parsed) == 0:
-        print('Could not parse songs from {}.'.format(songs_file.name))
-        print('Check that it is formatted correctly and try again.')
+    for i, song in enumerate(songs_to_download):
+        download_song(i + 1, total, song)
+
+    print
+
+
+def parse_songs_file(songs_file):
+    print('Parsing {} for songs to download'.format(songs_file.name))
+
+    songs = list(csv.DictReader(songs_file, delimiter="\t", quotechar='"'))
+    total = len(songs)
+
+    if total == 0:
+        print('Could not parse songs from {}, exiting'.format(songs_file.name))
         exit(1)
 
-    for row in parsed:
-        print(row)
+    print('Found {} song{} to download'.format(total, '' if total == 1 else 's'))
+
+    return songs
+
+
+def download_song(i, total, song):
+    print('[{}/{}] Downloading {} - {} from {}'.format(
+        i, total, song['Artist'], song['Title'], song['Source']
+    ))
