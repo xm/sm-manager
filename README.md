981b3a6d3ea257cf7c6bef667cecd5ee7af053bb
xm <xianmoniz@gmail.com>
2020-03-22 01:02:39 -0700

    Implement scan_for_disk for Windows and add requirements.txt

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 SM Manager
 ==========
 
-A simple tool used to manage and load songs onto any drive to use with the karaoke machines made by Singing Machine. 
+A simple tool used to manage and load songs onto any drive to use with the karaoke machines made by Singing Machine.
 
 It supports managing your meta data, as well as downloading and importing songs from YouTube.
 
@@ -12,11 +12,22 @@ Table of Contents
 <!--ts-->
    * [SM Manager](#sm-manager)
    * [Table of Contents](#table-of-contents)
+   * [Requirements](#requirements)
    * [Usage](#usage)
      * [--doctor](#--doctor)
      * [--import \<file\>](#--import-)
      * [--disk-root \<path\>](#--disk-root-)
 <!--te-->
+
+
+Requirements
+============
+
+Here is a list of things needed to run this script:
+  * Python3 -- [website](https://www.python.org/about/gettingstarted/)
+  * Run `pip install -r requirements.txt` to install the following dependencies:
+    * pytube -- [website](https://github.com/nficano/pytube)
+    * pywin32 (for Windows users) -- [website](https://pypi.org/project/pywin32/)
 
 
 Usage
@@ -36,3 +47,5 @@ TODO
 
 --disk-root <path>
 ------------------
+
+TODO
--- lib/paths.py
+++ lib/paths.py
@@ -7,7 +7,7 @@ from lib.templates import NEW_INFO_HEAD_FILE, NEW_PLAYLISTS_FILE, \
 # Path templates which are initialized first thing in main.py
 # NOTE: Do not prefix these with '/'!
 DATA_ROOT = 'karaoke'                # directory of MP4 video files
-META_ROOT = 'karaokeinfo'            # directory *.ini files 
+META_ROOT = 'karaokeinfo'            # directory *.ini files
 INFO_HEAD_FILE = 'info_head.ini'     # meta file that keeps track of song counts
 PLAYLISTS_FILE = 'playlist.ini'      # meta file that keeps track of playlists
 FAVORITES_FILE = 'favorite.ini'      # meta file that keeps track of favorite songs
@@ -18,7 +18,7 @@ SONG_META_FILE = 'ID{index:04}.ini'  # song meta file
 def init_paths(disk_root=None):
   global DATA_ROOT, META_ROOT, INFO_HEAD_FILE, PLAYLISTS_FILE, \
     FAVORITES_FILE, SONG_META_FILE
-  
+
   if disk_root is not None:
     if not path.exists(disk_root):
       print('Passed in root path \'{}\' does not exist, exiting'.format(disk_root))
@@ -40,22 +40,46 @@ def init_paths(disk_root=None):
   bootstrap_disk()
 
 
-# Attempts to find the disk root by looking for ../karaokeinfo/info_head.ini 
+# Attempts to find the disk root by looking for ../karaokeinfo/info_head.ini
 def scan_for_disk_root():
-  is_mac = system() == 'Darwin'
-  search_dir = None
+  os_name = system()
 
-  if is_mac:
-    search_dir = '/Volumes'
-  else:
-    return None
+  if os_name == 'Darwin':  # Mac
+    root_disk = scan_for_disk_root_mac()
+  elif os_name == 'Windows':
+    root_disk = scan_for_disk_root_win()
 
+  if root_disk is not None:
+    print('Found Singing Machine disk: {}'.format(root_disk))
+
+  return root_disk
+
+
+has_info_head = lambda root: path.exists(path.join(root, META_ROOT, INFO_HEAD_FILE))
+
+
+# Mac implementation of scan_for_disk
+def scan_for_disk_root_mac():
+  search_dir = '/Volumes'
   sub_dirs = next(walk(search_dir))[1]
   candidates = map(lambda d: path.join(search_dir, d), sub_dirs)
-  has_info_head = lambda d: path.exists(path.join(d, META_ROOT, INFO_HEAD_FILE))
   disk_root = next(iter(filter(has_info_head, candidates)), None)
-  
+
   return disk_root
+
+
+# Windows implementation of scan_for_disk
+def scan_for_disk_root_win():
+  from win32api import GetLogicalDriveStrings
+  from win32file import GetDriveType, DRIVE_FIXED, DRIVE_REMOVABLE
+
+  drives = GetLogicalDriveStrings()
+  drives = drives.split('\000')[:-1]
+  drives = list(filter(lambda d: GetDriveType(d) == DRIVE_FIXED or GetDriveType(d) == DRIVE_REMOVABLE, drives))
+  disk_root = next(iter(filter(has_info_head, drives)), None)
+
+  return disk_root
+
 
 # Prompts for manual entry of the disk root
 def prompt_for_disk_root(show_header=True):
@@ -92,7 +116,7 @@ def bootstrap_disk():
   if prompt != 'y':
     print('Aborting due to missing files on your disk')
     exit(1)
-  
+
 
   for p in paths_to_create:
     print('Creating directory {}'.format(p))
