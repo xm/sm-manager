97a1a1479e595aee5cfa2e4597330f325af0c3d0
xm <xianmoniz@gmail.com>
2020-03-22 02:56:49 -0700

    Reorder write_song_meta_file args

--- lib/sm.py
+++ lib/sm.py
@@ -20,12 +20,12 @@ def write_info_head(song_count):
 
     # write to file and truncate
     kinfo.write(templates.INFO_HEAD_TEMPLATE.format(
-      fs_hh=data['filesize_HH'], 
-      fs_hl=data['filesize_HL'], 
-      fs_lh=data['filesize_LH'], 
-      fs_ll=data['filesize_LL'], 
-      count=song_count, 
-      version=data['version'], 
+      fs_hh=data['filesize_HH'],
+      fs_hl=data['filesize_HL'],
+      fs_lh=data['filesize_LH'],
+      fs_ll=data['filesize_LL'],
+      count=song_count,
+      version=data['version'],
       machine=data['machine_id']
     ))
     kinfo.truncate()
@@ -34,10 +34,10 @@ def write_info_head(song_count):
     kinfo.seek(0)
     print('info_head.ini updated:')
     print(kinfo.read())
- 
+
 
 def get_song_meta_files():
-  # get data files sorted by modified time since this is how Singing Machine associates it 
+  # get data files sorted by modified time since this is how Singing Machine associates it
   # to the respective meta file. for example: ID0000.ini will be associated with the oldest
   # modified video in DATA_ROOT, and ID0001.ini will be associated with the second oldest
   # modified video, and so on.
@@ -45,14 +45,14 @@ def get_song_meta_files():
     lambda p: p.is_file() and not p.name.startswith('.'),
     sorted(pathlib.Path(paths.DATA_ROOT).iterdir(), key=os.path.getmtime))
   song_meta_files = []
-  
+
   for i, data_path in enumerate(sorted_data_paths):
     song_meta_files.append({
       'data_path': data_path,
       'meta_path': paths.SONG_META_FILE.format(index=i),
       'meta': read_song_meta_file(i)
     })
-  
+
   return song_meta_files
 
 
@@ -60,16 +60,19 @@ def read_song_meta_file(index):
   path = paths.SONG_META_FILE.format(index=index)
 
   try:
-    with open(path) as file: 
+    with open(path) as file:
       return json.loads(file.read())
   except FileNotFoundError:
     return None
 
 
-def write_song_meta_file(index, artist, title, genre):
+def write_song_meta_file(index, genre, artist, title):
   path = paths.SONG_META_FILE.format(index=index)
 
   with open(path, 'w') as file:
     file.write(templates.SONG_META_TEMPLATE.format(
-      artist=artist, title=title, genre=genre))
+      genre=genre, artist=artist, title=title
+    ))
     file.truncate()
+
+  return path
